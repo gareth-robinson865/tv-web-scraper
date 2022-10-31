f2a9a468be2cea2fb20823c5189c023f81332857
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-10-31 15:25:28 +0000

    variable created to hold the url for the site

--- scraper.js
+++ scraper.js
@@ -1,2 +1,4 @@
 const axios = require('axios');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
+
+const url = 'https://www.amazon.co.uk/Samsung-OLED-Built-LaserSlim-Ultrawide/dp/B09YMFT5MQ'
