89d225dc6f88b6a3620bebfee6ff439d9afbb595
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-11-01 15:56:34 +0000

    function outline

--- scraper.js
+++ scraper.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const url = 'https://www.amazon.co.uk/Samsung-OLED-Built-LaserSlim-Ultrawide/dp/B09YMFT5MQ'
+const url = 'https://www.amazon.co.uk/Samsung-OLED-Built-LaserSlim-Ultrawide/dp/B09YMFT5MQ';
+
+async function scrape() {
+    
+}
