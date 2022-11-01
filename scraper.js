3eb1824014dcbafcbad09dc8ad476ef0a83df5f1
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-11-01 15:59:08 +0000

    variable created to take the data from the url

--- scraper.js
+++ scraper.js
@@ -4,5 +4,6 @@ const cheerio = require('cheerio');
 const url = 'https://www.amazon.co.uk/Samsung-OLED-Built-LaserSlim-Ultrawide/dp/B09YMFT5MQ';
 
 async function scrape() {
-    
+    //Fetching the data
+    const { data } = await axios.get(url);
 }
