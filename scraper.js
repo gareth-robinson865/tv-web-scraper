509e3487fe1cd92538aac8678f05ea53c3c44df0
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-11-03 19:58:57 +0000

    variable created with account SID and auth token for twilio

--- scraper.js
+++ scraper.js
@@ -3,6 +3,8 @@ const cheerio = require('cheerio');
 require('dotenv').config();
 const accountSID = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN
+const client = require('twilio')(accountSID, authToken);
+
 const url = 'https://www.amazon.co.uk/Samsung-OLED-Built-LaserSlim-Ultrawide/dp/B09YMFT5MQ';
 
 //create an object to hold the product information
